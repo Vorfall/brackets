5044e826bf17ddb1704d7a046462cbf684164978
Vorfall <alekseevegorc@gmail.com>
2021-03-10 12:27:46 +0300

    init:Start solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   flag = true;
+    while(flag){
+        flag=false;
+      for(e of bracketsConfig){
+          brackets = e[0]+e[1];
+          while(str.includes(brackets)){
+                  str = str.slice(0,str.indexOf(brackets)) + str.slice(str.indexOf(brackets)+brackets.length);
+                  flag=true;
+          }
+      }
+  }
+  if(str)
+      return false;
+  else
+      return true;
 }
